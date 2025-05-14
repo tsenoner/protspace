c85207c17c0b471e28fe8466851ee74479a9ce37
heispv <peymanvahidi1998@gmail.com>
2025-05-14 19:52:43 +0200

    Improved ProteinFeatureExtractor class, added batch size for request

--- apps/protspace/src/protspace/utils/generate_csv.py
+++ apps/protspace/src/protspace/utils/generate_csv.py
@@ -1,70 +1,80 @@
-import logging
-from typing import List, Union
-from pathlib import Path
 import csv
-from bioservices import UniProt
 import pandas as pd
+from typing import List
+from pathlib import Path
+from bioservices import UniProt
+
+import logging
 
 logging.basicConfig(format="%(levelname)s: %(message)s")
 logger = logging.getLogger(__name__)
 
 # UniProt features
-FEATURES = ['id', 'accession', 'protein_existence', 'annotation_score']
+# TODO: Add more features
+FEATURES = ['protein_existence', 'annotation_score']
 
 class ProteinFeatureExtractor:
     def __init__(
         self,
-        protein_ids: Union[List[str], Path],
+        headers: List[str],
         features: List,
         csv_output: Path
     ):
-        self.protein_ids = protein_ids
+        self.headers = headers
         self.features = FEATURES if features is None else self._validate_features(features)
         self.u = UniProt(verbose=False)
-
         self.csv_output = csv_output
-        self._data = None
-    
 
     def to_pd(self) -> pd.DataFrame:
-        self.get_uniprot_features()
-        self.save_csv()
+        fetched_data = self.get_uniprot_features()
+        self.save_csv(fetched_data)
         return pd.read_csv(self.csv_output)
     
     def get_uniprot_features(self) -> str:
-        query = '+OR+'.join([f"id:{protein_id}" for protein_id in self.protein_ids])
-        columns = ','.join(self.features)
+        self.headers = self.headers[:10]
+        batch_size = 100
+        all_data = []
+        first_batch = True
         
-        data = self.u.search(
-            query=query,
-            columns=columns
-        )
-
-        self._data = data
+        for i in range(0, len(self.headers), batch_size):
+            batch = self.headers[i:i+batch_size]
+            query = '+OR+'.join([f"accession:{accession}" for accession in batch])
+            columns = ','.join(self.features)
+            
+            data = self.u.search(
+                query=query,
+                columns=columns
+            )
+            
+            if data:
+                if first_batch:
+                    all_data.append(data)
+                    first_batch = False
+                else:
+                    # Skip the header
+                    all_data.append('\n'.join(data.strip().split('\n')[1:]))
+        
+        fetched_data = '\n'.join(all_data) if all_data else ""
+        return fetched_data
     
-    def save_csv(self):
-            if not self._data:
-                self._get_uniprot_features()
-            
-            lines = self._data.strip().split('\n')
-            headers = lines[0].split('\t')
-            
-            headers = ['identifier' if h == 'Entry Name' else h for h in headers]
-            data_rows = [line.split('\t') for line in lines[1:]]
-            
-            with open(self.csv_output, 'w', newline='') as f:
-                writer = csv.writer(f)
-                writer.writerow(['identifier'] + [h for h in headers if h != 'identifier'])
-                for row in data_rows:
-                    writer.writerow(row)
+    def save_csv(self, fetched_data: str):
+        lines = fetched_data.strip().split('\n')
+        
+        csv_headers = ["identifier"] + self.features[1:] # Skip "accession" from self.features
+        data_rows = [line.split('\t') for line in lines[1:]]
+        
+        with open(self.csv_output, 'w', newline='') as f:
+            writer = csv.writer(f)
+            writer.writerow(csv_headers)
+            for row in data_rows:
+                writer.writerow(row)
         
     def _validate_features(self, features: List) -> str:
-        print(features)
         for feature in features:
             if feature not in FEATURES:
                 raise ValueError(f"Feature {feature} not found in feature map")
         
-        if "id" in features:
-            features.remove("id")
+        if "accession" in features:
+            features.remove("accession")
 
-        return ["id"] + features
+        return ["accession"] + features
